4ba20e5c9cd7e847e6b21815c951466876cff265
mika <2everlove@gmail.com>
2023-12-21 20:22:48 +0900

    bfs dfs

--- codingTest/StringExample.java
+++ codingTest/StringExample.java
@@ -1,7 +1,9 @@
 package codingTest;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class StringExample {
 
@@ -43,6 +45,41 @@ public class StringExample {
 			start ++;
 			end --;
 		}
+	}
+	
+	public static boolean[] visited = new boolean[9];
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+	
+    public static void dfs(int x) {
+	    // 현재 노드를 방문 처리
+	    visited[x] = true;
+	    System.out.print(x + " ");
+	    // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+	    for (int i = 0; i < graph.get(x).size(); i++) {
+	        int y = graph.get(x).get(i);
+	        if (!visited[y]) dfs(y);
+	    }
+	}
+	
+	public static void bfs(int start) {
+	    Queue<Integer> q = new LinkedList<>();
+	    q.offer(start);
+	    // 현재 노드를 방문 처리
+	    visited[start] = true;
+	    // 큐가 빌 때까지 반복
+	    while(!q.isEmpty()) {
+	        // 큐에서 하나의 노드를 뽑아 출력
+	        int x = q.poll();
+	        System.out.print(x + " ");
+	        // 해당 노드와 연결된, 아직 방문하지 않은 노드들을 큐에 삽입
+	        for(int i = 0; i < graph.get(x).size(); i++) {
+	            int y = graph.get(x).get(i);
+	            if(!visited[y]) {
+	                q.offer(y);
+	                visited[y] = true;
+	            }
+	        }
+	    }
 	}
 	
 	public static String[] reorderLogFiles(String[] logs) {
